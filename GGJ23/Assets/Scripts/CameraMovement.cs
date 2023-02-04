f0deeb24c601c2729b503edcfe96a946259f5a1b
BeastieBaiter <62881476+BeastieBaiter@users.noreply.github.com>
2023-02-04 15:15:49 +0000

    Camera Scroll finished

---
+++ GGJ23/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Vector3 _cameraPosition;
+    [SerializeField] private float cameraSpeed;
+    [SerializeField] private float topBoundry, bottomBoundry;
+    
+    void Start()
+    {
+        _cameraPosition = this.transform.position;
+    }
+
+    void Update()
+    {
+        _cameraPosition.y += Input.GetAxisRaw("Vertical") * (cameraSpeed / 100);
+        if (_cameraPosition.y > topBoundry || _cameraPosition.y < bottomBoundry) return;
+        transform.position = _cameraPosition;
+    }
+}
